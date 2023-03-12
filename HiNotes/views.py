73120c287648f1e8b1fb601bf2445858c4ac40d9
Chirag Jain <86999299+chiragj-coder@users.noreply.github.com>
2023-03-12 15:22:41 +0530

    Update views.py

--- HiNotes/views.py
+++ HiNotes/views.py
@@ -2,13 +2,7 @@ from django.shortcuts import render
 import pyrebase
 
 config={
-    "apiKey": "AIzaSyA4FIcyNcNjYP_Z8Ll4VyO4XAs23cVeL4E",
-    "authDomain": "easenotes-cj.firebaseapp.com",
-    "databaseURL": "https://easenotes-cj-default-rtdb.asia-southeast1.firebasedatabase.app/data",
-    "projectId": "easenotes-cj",
-    "storageBucket": "easenotes-cj.appspot.com",
-    "messagingSenderId": "741857872370",
-    "appId": "1:741857872370:web:f269e26efdd448b911836b"
+
 }
 
 firebase=pyrebase.initialize_app(config)
