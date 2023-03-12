1a234fe91317e37fa963925ef4bbdd1e70e55fe1
Chirag Jain <86999299+chiragj-coder@users.noreply.github.com>
2023-03-12 15:23:06 +0530

    Update settings.py

--- HiNotes/settings.py
+++ HiNotes/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-grff395r9k0qnx%&zbqs0dr33z2e9w!x^iam-^yw@_!z8rar=='
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
